65bf3553cd3f74d15e361481f4c7316e3d339710
Nipuna Weerasekara <w.nipuna@gmail.com>
2019-08-03 11:44:33 +0530

    Major updates
    Released the GoogleDict API v2

--- app.js
+++ app.js
@@ -1,125 +1,16 @@
 const express = require("express");
-const cheerio = require("cheerio");
-const request = require("request");
+const path = require("path");
+var favicon = require("serve-favicon");
+
+const v1Main = require("./routes/v1/main");
+const v2Main = require("./routes/v2/main");
 
 const app = express();
+app.use(favicon(path.join(__dirname, "./favicon.png")));
 
-app.get("/:word", function(req, res) {
-  const queriedWord = req.params.word.split(/[^A-Za-z]/)[0];
-
-  if (!queriedWord) {
-    res.send(
-      JSON.stringify({
-        error: "No word is given!",
-        endpoint: "https://dict.niweera.gq/{word}",
-        note: "Only use a single word for queries"
-      })
-    );
-  } else {
-    console.log(queriedWord);
-
-    var url;
-
-    url = `https://www.google.com/search?&q=define+${queriedWord}`;
-    url = encodeURI(url);
-    request(
-      {
-        method: "GET",
-        url: url,
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0"
-        }
-      },
-      function(err, response, body) {
-        if (err) {
-          return console.error(err);
-        }
-
-        const $ = cheerio.load(body);
-
-        var dictionary = {},
-          word = $("div.dDoNo span")
-            .first()
-            .text(),
-          definition,
-          mainPart,
-          defns;
-
-        if (word.length < 1) {
-          res.header("Access-Control-Allow-Origin", "*");
-          return res.status(404).send(
-            JSON.stringify({
-              error: "Cannot define the given word"
-            })
-          );
-        }
-
-        var queryWord = "";
-        queryWord = $("div.dDoNo span")
-          .first()
-          .text();
-
-        dictionary.word = queryWord.replace(/[^A-Za-z]/g, "");
-
-        dictionary.definition = "";
-
-        (definition = ""),
-          (defns = $(".lr_dct_ent.vmod.XpoqFe")),
-          (mainPart = defns.first().find(".lr_dct_sf_h"));
-
-        mainPart.each(function(i, element) {
-          var selector = $(".lr_dct_sf_sens")
-            .eq(i)
-            .find("div[style='margin-left:20px'] > .PNlCoe");
-
-          selector.each(function(i, element) {
-            var newDefinition = "";
-
-            newDefinition = $(this)
-              .find("div[data-dobid='dfn']")
-              .text();
-
-            definition = newDefinition;
-            return false;
-          });
-          return false;
-        });
-
-        dictionary.definition = definition;
-
-        Object.keys(dictionary).forEach(key => {
-          Array.isArray(dictionary[key]) &&
-            !dictionary[key].length &&
-            delete dictionary[key];
-        });
-
-        res.header("Content-Type", "application/json");
-        res.header("Access-Control-Allow-Origin", "*");
-        res.send(JSON.stringify(dictionary, null, 4));
-      }
-    );
-  }
-});
-
-app.get("/", (req, res) => {
-  res.send(
-    JSON.stringify({
-      error: "No word is given!",
-      endpoint: "https://dict.niweera.gq/{word}",
-      note: "Only use a single word for queries"
-    })
-  );
-});
-
-const fourNaughtFour = {
-  error: "Invalid Endpoint!",
-  endpoint: "https://dict.niweera.gq/{word}",
-  note: "Only use a single word for queries"
-};
-app.get("*", function(req, res) {
-  res.status(404).json(fourNaughtFour);
-});
+// Use Routes
+app.use("/v2", v2Main);
+app.use("/", v1Main); //this route must be at the end
 
 // Start the server
 const PORT = process.env.PORT || 8080;
---
+++ routes/v2/main.js
@@ -0,0 +1,135 @@
+const express = require("express");
+const router = express.Router();
+const cheerio = require("cheerio");
+const request = require("request");
+
+// @route   GET /v2
+// @desc    Root endpoint for API v2
+// @access  Public
+router.get("/", (req, res) => {
+  res.send(
+    JSON.stringify({
+      error: "No word is given!",
+      endpoint: "https://dict.niweera.gq/v2/{word}",
+      note: "Only use a single word for queries"
+    })
+  );
+});
+
+// @route   GET /v2/:word
+// @desc    Get definition for the word given
+// @access  Public
+router.get("/:word", function(req, res) {
+  const queriedWord = req.params.word.split(/[^A-Za-z]/)[0];
+
+  if (!queriedWord) {
+    res.send(
+      JSON.stringify({
+        error: "No word is given!",
+        endpoint: "https://dict.niweera.gq/v2/{word}",
+        note: "Only use a single word for queries"
+      })
+    );
+  } else {
+    console.log(queriedWord);
+
+    var url;
+
+    url = `https://www.google.com/search?&q=define+${queriedWord}`;
+    url = encodeURI(url);
+    request(
+      {
+        method: "GET",
+        url: url,
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0"
+        }
+      },
+      function(err, response, body) {
+        if (err) {
+          return console.error(err);
+        }
+
+        const $ = cheerio.load(body);
+
+        var dictionary = {},
+          word = $("div.dDoNo span")
+            .first()
+            .text(),
+          definition,
+          mainPart,
+          defns;
+
+        if (word.length < 1) {
+          res.header("Access-Control-Allow-Origin", "*");
+          return res.status(404).send(
+            JSON.stringify({
+              error: "Cannot define the given word"
+            })
+          );
+        }
+
+        var queryWord = "";
+        queryWord = $("div.dDoNo span")
+          .first()
+          .text();
+
+        dictionary.word = queryWord.replace(/[^A-Za-z]/g, "");
+
+        dictionary.definition = {};
+
+        (definition = {}),
+          (defns = $(".lr_dct_ent.vmod.XpoqFe")),
+          (mainPart = defns.first().find(".lr_dct_sf_h"));
+
+        mainPart.each(function(i, element) {
+          var type = $(this)
+              .find("i")
+              .text(),
+            selector = $(".lr_dct_sf_sens")
+              .eq(i)
+              .find("div[style='margin-left:20px'] > .PNlCoe");
+
+          definition[type] = [];
+
+          selector.each(function(i, element) {
+            var newDefinition = "";
+
+            newDefinition = $(this)
+              .find("div[data-dobid='dfn']")
+              .text();
+
+            definition[type].push(newDefinition);
+          });
+        });
+
+        dictionary.definition = definition;
+
+        Object.keys(dictionary).forEach(key => {
+          Array.isArray(dictionary[key]) &&
+            !dictionary[key].length &&
+            delete dictionary[key];
+        });
+
+        res.header("Content-Type", "application/json");
+        res.header("Access-Control-Allow-Origin", "*");
+        res.send(JSON.stringify(dictionary, null, 4));
+      }
+    );
+  }
+});
+
+// @route   GET /v2/*
+// @desc    Return 404 for all unidentified routes
+// @access  Public
+const fourNaughtFour = {
+  error: "Invalid Endpoint!",
+  endpoint: "https://dict.niweera.gq/v2/{word}",
+  note: "Only use a single word for queries"
+};
+router.get("*", function(req, res) {
+  res.status(404).json(fourNaughtFour);
+});
+
+module.exports = router;
