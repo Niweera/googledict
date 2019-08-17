2323e90b43476c520f78dc950c86af9ed96d6409
Nipuna Weerasekara <w.nipuna@gmail.com>
2019-08-17 16:39:59 +0530

    add test

---
+++ test/test-pages.js
@@ -0,0 +1,35 @@
+//Require the dev-dependencies
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = "https://dict.niweera.gq";
+let should = chai.should();
+
+chai.use(chaiHttp);
+//Our parent block
+describe("Dict/hello", () => {
+  /*
+   * Test the /GET route
+   */
+
+  describe("/GET hello", () => {
+    it("It should GET the definition of hello", done => {
+      chai
+        .request(server)
+        .get("/hello")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a("object");
+          res.body.should.have.property("word");
+          res.body.word.should.be.a("string");
+          res.body.word.should.equal("hello");
+          res.body.should.have.property("definition");
+          res.body.definition.should.be.a("string");
+          res.body.definition.should.equal(
+            "used as a greeting or to begin a telephone conversation."
+          );
+          done();
+        });
+    });
+  });
+});
