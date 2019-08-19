ae80622d0b7922915e9bc01b11cf2fa75242516e
Nipuna Weerasekara <w.nipuna@gmail.com>
2019-08-19 14:06:50 +0530

    update tests

--- test/test-pages.js
+++ test/test-pages.js
@@ -7,7 +7,8 @@ let should = chai.should();
 chai.use(chaiHttp);
 //Our parent block
 describe("Dict/hello", () => {
-   /*
+  beforeEach(done => setTimeout(done, 180000));
+  /*
    * Test the /GET route
    */
 
